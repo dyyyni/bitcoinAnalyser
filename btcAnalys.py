c22ad815bac0e19086b9439c6b87bd8a9e332973
dyyyni <luomadaniel95@gmail.com>
2021-12-08 14:10:46 +0200

    btcAnalys now only returns the result on succesfull usage

--- btcAnalys.py
+++ btcAnalys.py
@@ -35,10 +35,10 @@ def isDateValid(parsedDate):
         # datetime requires the form yyyy/dd/mm
         date = datetime.datetime(int(parsedDate[2]), int(parsedDate[1]), int(parsedDate[0]))
         if date > dateNow:
-            print(f"As of now, I can't see to the future. The 'to date' is set to {dateNow.strftime('%d/%m/%Y')}.")
+            # As of now, I can't see to the future. The 'to date' is set to current date
             date = dateNow
         if date < dateLowLimit:
-            print(f"My data sources only reach to 28/4/2013. It's set as the 'from date'.")
+            # My data sources only reach to 28/4/2013. It's set as the 'from date'.
             date = dateLowLimit
         utcTimestamp = date.replace(tzinfo=datetime.timezone.utc).timestamp()
         return utcTimestamp
